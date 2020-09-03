5cc222fd1d31f538624792634b34e0d5b36a7348
Tiramisu <tiramisu@Tiramisus-MacBook-Pro.local>
2020-09-03 11:20:28 +0700

    Add reverb API

--- flask_add_reverb.py
+++ flask_add_reverb.py
@@ -3,6 +3,15 @@ import requests
 from flask import Flask, send_file, escape, request, render_template, jsonify, make_response, session
 import random
 from flask_cors import CORS
+from AudioLib.AudioProcessing import AudioProcessing
+import pathlib
+
+project_root = pathlib.Path(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir)
+))
+
+
+
 app = Flask(__name__)
 CORS(app)
 @app.route('/')
@@ -23,6 +32,20 @@ def upload():
         res = make_response(jsonify({"file_path": file_path, "message": "Đã lưu file : {} lên server".format(filename)}))
         return res
     return render_template('index.html')
+
+@app.route('/add_reverb/<file_path>')
+def add_reverb(file_path):
+    print(file_path)
+    file_path = file_path.replace('=', '/')
+    file_name = '.'.join(file_path.split('.')[0:-1])
+    sound1 = AudioProcessing(str(file_path))
+    sound1.set_reverb(0.2, 0.5)
+    out_file_path = file_name + '_added_reverb.wav'
+    sound1.save_to_file(str(out_file_path))
+    print('add reverb done!!')
+    print (out_file_path)
+    res = make_response(jsonify({"out_file_path":out_file_path, "message": "Thêm tiếng vọng hoàn tất!"}))
+    return res, 200
 
 if __name__ == "__main__":
     app.debug = True
--- flask_server.py
+++ flask_server.py
@@ -1,18 +1,17 @@
-
-
 import os
+import io
 import requests
-from flask import Flask, send_file, escape, request, render_template, jsonify, make_response, session
+from flask import Flask, send_file, escape, request, render_template, jsonify, make_response, session,send_from_directory
 #from utils import cvtToWavMono16, split
-
 import random
-from infer_file import get_model
 from flask_cors import CORS
+from infer import get_model_new
+UPLOAD_DIRECTORY = "./upload"
 
 app = Flask(__name__)
 CORS(app)
-infer = get_model()
-
+infer = get_model_new()
+ 
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -27,6 +26,14 @@ def upload():
             return render_template('index.html', filename='{} file not support. select mp3, wav or flac!'.format(filename))      
         file_path = 'static/upload/' + filename
         file.save(file_path)
+        #if  os.path.splitext(file_path)[1][1:].strip() not in ['wav']:
+        #    out_file_path =u'"%s.wav"' %(file_path[:-4])
+        #    print ('out_file_path:',out_file_path)
+        #    str =u'ffmpeg -i "%s" -acodec pcm_u8 -ar 16000 -ac 1 "%s"' %(file_path,out_file_path)
+        #    print (str)
+        #    os.system(str)
+        #    file_path =out_file_path
+
         print('saved file: {}'.format(file_path))
         res = make_response(jsonify({"file_path": file_path, "message": "Đã lưu file : {} lên server".format(filename)}))
         return res
@@ -38,12 +45,27 @@ def predict(file_path):
     file_path = file_path.replace('=','/')
     out_file_path = infer(file_path)
     print('predict done!!')
-    print (file_path)
+    print('out_file_path:', out_file_path)
     res = make_response(jsonify({"out_file_path":out_file_path, "message": "Khử nhiễu hoàn tất!"}))
     return res
+
+@app.route("/audio", methods=["POST"])
+def process_audio():
+    file = request.files['audio']
+    file_path = os.path.join('./upload/', file.filename)
+    file.save(file_path)
+    out_file_path = infer(file_path)
+    with open(out_file_path, 'rb') as bites:
+        return send_file(io.BytesIO(bites.read()),attachment_filename='out.wav',mimetype='audio/wav')
+@app.route("/audio_url", methods=["POST"])
+def process_audio_url():
+    file = request.files['audio']
+    file_path = os.path.join('./upload/', file.filename)
+    file.save(file_path)
+    out_file_path = infer(file_path)
+    return jsonify({'url':     out_file_path})
 
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = 'dangvansam'
     app.run(host='192.168.1.254', port='9002')
-    #app.run(port='8080')
